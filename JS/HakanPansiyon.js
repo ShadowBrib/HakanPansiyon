272eaa529ec7ee9fb164be37141a2b34f788499b
Enes Çalış <bgenes145@gmail.com>
2025-05-21 19:40:38 +0300

    hataları düzelttim ve projemin sonuna geldim

--- JS/HakanPansiyon.js
+++ JS/HakanPansiyon.js
@@ -1,26 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
 // URL'den oda ID'sini al
 const params = new URLSearchParams(window.location.search);
 const odaId = params.get("id");
 
 // Oda bilgileri
 const odaBilgileri = {
-    "oda1": {
-        baslik: "Oda 1 - Tek Kişilik",
+    "1": {
+        baslik: "Oda 1 - Aile Odası",
         aciklama: "Bu oda tek kişilik konaklamalar için uygundur. TV, Wi-Fi, klima ve özel banyo mevcuttur.",
-        fiyat: "Gecelik: 250 TL",
-        resim: "../resimler/oda 1.jpg"
+        fiyat: "Gecelik: 500 TL",
+        resim: "../resimler/oda 1.jpg",
+        yorumsahibi: "FATİH T.",
+        yorum: "Temiz odalar ve harika konum! Personel çok yardımcıydı."
+        
     },
-    "oda2": {
+    "2": {
         baslik: "Oda 2 - Çift Kişilik",
         aciklama: "Geniş çift kişilik yataklı oda. Balkon, Wi-Fi, klima ve banyo içerir.",
         fiyat: "Gecelik: 350 TL",
-        resim: "../resimler/oda 2.jpg"
+        resim: "../resimler/oda 2.jpg",
+        yorumsahibi: "OKAN B.",
+        yorum: "Fiyat-performans açısından mükemmel bir pansiyon. Tavsiye ederim."
     },
-    "oda3": {
+    "3": {
         baslik: "Oda 3 - Aile Odası",
         aciklama: "Aileler için ideal. 1 çift kişilik + 2 tek kişilik yatak. Klima, mini buzdolabı, banyo.",
         fiyat: "Gecelik: 500 TL",
-        resim: "../resimler/oda 3.jpg"
+        resim: "../resimler/oda 3.jpg",
+        yorumsahibi: "ARDA T.",
+        yorum: "Denize çok yakın, sabah kahvaltısı lezzetliydi."
+    },
+    "4": {
+        baslik: "Oda 4 - çift Kişilik",
+        aciklama: "Bu oda tek kişilik konaklamalar için uygundur. TV, Wi-Fi, klima ve özel banyo mevcuttur.",
+        fiyat: "Gecelik: 350 TL",
+        resim: "../resimler/oda 4.jpg",
+        yorumsahibi: "TUGBA A.",
+        yorum: "Tüm personele tek tek teşekkürlerimi iletiyorum. Mukemmel bir tatildi."
+    },
+    "5": {
+        baslik: "Oda 5 - Çift Kişilik",
+        aciklama: "Geniş çift kişilik yataklı oda. Balkon, Wi-Fi, klima ve banyo içerir.",
+        fiyat: "Gecelik: 350 TL",
+        resim: "../resimler/oda 5.jpg",
+        yorumsahibi: "ALTAY D.",
+        yorum: "Aileyele geçirilecek en güzel pansiyon diyebilirim."
     }
 };
 
@@ -39,3 +63,4 @@ if (oda) {
 } else {
     detayContainer.innerHTML = `<p>Geçersiz oda seçimi. Lütfen ana sayfaya dönün.</p>`;
 }
+});
